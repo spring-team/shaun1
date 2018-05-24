410143e40fc310cdbb2961b2eb9d0223513399c6
Rod Johnson <johnsonroda@gmail.com>
2018-05-25 09:21:32 +1000

    Update Shaun1Controller.java

--- src/main/java/com/atomist/Shaun1Controller.java
+++ src/main/java/com/atomist/Shaun1Controller.java
@@ -27,7 +27,7 @@ class Shaun1Controller {
 
     @RequestMapping(method = GET, path = "/")
     public String root() {
-        return "Spring Boot REST service running across the Pacific and continental USA, Served by " + getClass().getName();
+        return "Spring Boot REST service take 2, Served by " + getClass().getName();
     }
 
     @RequestMapping(method = GET, path = "hello/{name}")
